0707cdc9431ca4a4fbd3437e021c5e64ae081d00
Mackenzie Gray <mdkulen@gmail.com>
2022-07-27 11:03:06 +1000

    added post route for new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,4 +5,10 @@ const fs = require('fs');
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(userNotesData));
 
+    app.post('./api/notes', (req, res) => {
+        req.body['id'] = uniqid();
+        userNotesData.push(req.body);
+        res.json(true);
+        fs.writeFileSync('./db/db.json', JSON.stringify(userNotesData));
+    });
 }
