7df14c8a947016ea4605438cf80ac97cf6faa0af
Mackenzie Gray <mdkulen@gmail.com>
2022-07-28 14:37:50 +1000

    added helpers file with fsUtils.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,22 @@
 const userNotesData = require('../db/db.json');
 const uniqid = require('uniqid');
 const fs = require('fs');
+const { readFromFile, readAndAppend, writeToFile, } = require('../helpers/fsUtils');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(userNotesData));
 
-    app.post('./api/notes', (req, res) => {
+    app.post('/api/notes', (req, res) => {
         req.body['id'] = uniqid();
         userNotesData.push(req.body);
         res.json(true);
         fs.writeFileSync('./db/db.json', JSON.stringify(userNotesData));
 
         console.log(`${req.method} request received`);
-        
+
         res.json(`${req.method} request received`);
     });
 }
+
+
+// Generate helper function folder and write functions to readFile etc then link that page to top of page
