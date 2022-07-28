1631f04d2dafd380b627c52f322888e4bf291cb2
Mackenzie Gray <mdkulen@gmail.com>
2022-07-28 14:37:04 +1000

    changed console.log return

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
