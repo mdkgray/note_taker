45de183f96156a96ff95ac0840df34370200f282
Mackenzie Gray <mdkulen@gmail.com>
2022-07-29 16:34:04 +1000

    updated console.log message

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,13 +4,13 @@ const path = require('path');
 //GET route for notes.html page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
-    console.log(`${req.method} request received`);
+    console.log(`${req.method} request received for notes.html`);
 });
 
 // wildcard GET route to direct users to index.html page 
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
-    console.log(`${req.method} request received`);
+    console.log(`${req.method} request received for index.html`);
 });
 
 module.exports = router;
